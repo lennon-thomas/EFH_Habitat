827530d3bc86902c3bfb6476a4a451bbba20283b
Lennon Thomas <lennonthomas@Lennons-MacBook-Air.local>
2018-03-27 00:11:49 -0700

    suitability analysis substrate data added

--- analysis.R
+++ analysis.R
@@ -43,7 +43,8 @@ tm_shape(hawaii_fish) +
   tm_shape(land)+
   tm_fill(col = "white") +
   tm_legend(main.title.size = 2, text.size = 1, position = c("right","top"),main.title = "Kona crab landings- All")+
-  tm_grid()
+  tm_grid() +
+  tm_text("AREA_ID")
 
 # kona_crab_public<-read.csv(paste0(boxdir,"/kcrab.csv")) 
 # colnames(kona_crab_public)<-c("area","Year","Lic","landings")
@@ -53,12 +54,54 @@ tm_shape(hawaii_fish) +
 #                     summarise(total_landings=sum(landings))
 # 
 # kona_crab_sp<-merge(hawaii,kona_crab_public,by.x="AREA_ID",by.y="area")
+hawaii_raster<-rasterize(hawaii,depth,field="AREA_ID")
+hawaii_fish_raster<-rasterize(hawaii_fish,depth,field = "total_landings",filename=paste0(boxdir,"hawaii_fish_raster.tif",overwrite = TRUE))
 
 depth<-raster(paste0(boxdir,"himbsyn.bathytopo.1km.v19.grd"))
 
 depth[depth>0]<-NA
 
-hawaii_fish_raster<-rasterize(hawaii_fish,depth,field = "total_landings",filename=paste0(boxdir,"hawaii_fish_raster.tif",overwrite = TRUE))
+depth[depth>-2]<-NA
+depth[-200>depth]<-NA
 
-substrate<-raster(paste0(boxdir,"hawaii_bs_msc/mhi_backscat_60m.nc"))
-substrate_reproj<-projectRaster(substrate,crs(hawaii),res=c(60,60))
+area_depth<-area(depth,na.rm = TRUE)
+
+depth_per_zone<-as.data.frame(zonal(area_depth,hawaii_raster,fun = 'sum',na.rm = TRUE)) %>%
+  dplyr::mutate(depth_suitabiilty= ifelse(sum==0,0,1))
+
+colnames(depth_per_zone)<-c("AREA_ID","depth_area","depth_suitability")
+
+
+substrate<-raster(paste0(boxdir,"MHI_backscatterSynthesis/mhi_backscat_60m.nc"))
+substrate_reproj<-projectRaster(substrate,hawaii_raster)
+
+substrate_reproj[substrate_reproj>140]<-NA
+area_substrate<-area(substrate_reproj,na.rm = TRUE)
+substrate_per_zone<-as.data.frame(zonal(area_substrate,hawaii_raster,fun='sum',na.rm=TRUE))
+colnames(substrate_per_zone)<-c("AREA_ID","substrate_area")
+
+area_substrate[is.na(area_substrate)]<-0
+area_depth[is.na(area_depth)]<-0
+area_depth[area_depth>1]<-1
+
+suit<-stack(area_substrate,area_depth)
+suit_final<-stackApply(suit,indices=c(1,1),fun=prod,na.rm = TRUE)
+
+suit_zonal<-zonal(suit_final,hawaii_raster,fun='sum',na.rm = TRUE)
+colnames(suit_zonal)<-c("AREA_ID","Suitable_area")
+
+all_data<-merge(suit_zonal,depth_per_zone,by="AREA_ID")
+all_data<-merge(all_data,substrate_per_zone,by = "AREA_ID")
+landings<-hawaii_fish@data[,c("AREA_ID","total_landings")]
+all_data<-merge(all_data,landings,by = "AREA_ID")
+
+
+tm_shape(hawaii) +
+  tm_fill(col = "lightblue",alpha=0.3) +
+ # tm_text("AREA_ID",size=0.5)+
+  tm_borders(lwd = 1.2) +
+  tm_shape(land)+
+  tm_fill(col = "white") +
+  
+  tm_shape(depth) +
+  tm_raster()
